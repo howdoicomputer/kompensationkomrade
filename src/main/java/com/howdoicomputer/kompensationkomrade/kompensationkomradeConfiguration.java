542b1bf6c95adb1c72062264cd45de2d50fb3c12
Tyler Hampton <dr.frankinfurter@gmail.com>
2016-08-05 21:15:42 -0700

    initial commit

---
+++ src/main/java/com/howdoicomputer/kompensationkomrade/kompensationkomradeConfiguration.java
@@ -0,0 +1,10 @@
+package com.howdoicomputer.kompensationkomrade;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class kompensationkomradeConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
